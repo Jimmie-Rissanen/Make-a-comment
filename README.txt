4d6e2b8c5e746e7e0c519243060946a4ce629bbf
Jimmie <jimmieriss@gmail.com>
2022-07-12 19:57:29 +0200

    Stop using redux.

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 To use this app you will need to have postgres and node on you local machine. 
 
-Go ahead and create a database, with a user and a passoword in postgres. Go to App/example.env and fill out your creadentials. Rename the file to just .env. 
+Go ahead and create a database, with a user and a passoword in postgres. Make sure that postgres is up and running. Go to App/example.env and fill out your creadentials. Rename the file to just ".env". 
 
 Cd into the App folder and run npm start. Your server will now be running on your specified port. 
--- ui/src/components/Comments.js
+++ ui/src/components/Comments.js
@@ -1,51 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Comment from "./Comment";
 import Form from "./Form";
 
 function Comments() {
-  const db = [
-    {
-      id: 1,
-      name: "Jimmie",
-      content: "JabbaOOOOO",
-      parent: 0,
-      createdAt: "2022-07-09T20:22:16.385Z",
-      updatedAt: "2022-07-09T20:22:16.385Z",
-    },
-    {
-      id: 2,
-      name: "Jimmie",
-      content: "JabbaOOOOO",
-      parent: 0,
-      createdAt: "2022-07-09T20:29:09.498Z",
-      updatedAt: "2022-07-09T20:29:09.498Z",
-    },
-    {
-      id: 3,
-      name: "Korven",
-      content:
-        "Lorén hicksum dollar sallat Lorén hicksum dollar sallat Lorén hicksum dollar sallat Lorén hicksum dollar sallat Lorén hicksum dollar sallat Lorén hicksum dollar sallat Lorén hicksum dollar sallatLorén hicksum dollar sallatLorén hicksum dollar sallatLorén hicksum dollar sallatLorén hicksum dollar sallatLorén hicksum dollar sallat",
-      parent: 2,
-      createdAt: "2022-07-09T20:31:32.538Z",
-      updatedAt: "2022-07-09T20:31:32.538Z",
-    },
-    {
-      id: 4,
-      name: "Snickers",
-      content: "MHM",
-      parent: 1,
-      createdAt: "2022-07-09T20:31:51.243Z",
-      updatedAt: "2022-07-09T20:31:51.243Z",
-    },
-    {
-      id: 5,
-      name: "Tjenare",
-      content: "Japp",
-      parent: 0,
-      createdAt: "2022-07-09T20:32:06.849Z",
-      updatedAt: "2022-07-09T20:32:06.849Z",
-    },
-  ];
+  const db = [];
   const rootComments = db.filter((rootComment) => rootComment.parent === 0);
 
   const getReplies = (commentId) => {
